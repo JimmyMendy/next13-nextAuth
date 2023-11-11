51f2c12e5ac732d364ca51b15b413f5e3fa03430
JimmyMendy <jimmy.mendy@gmail.com>
2023-11-11 13:55:05 +0000

    half

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -1,44 +1,72 @@
 "use client";
 
-import { signIn } from 'next-auth/react'
-import { useRouter } from 'next/navigation';
-import { FormEvent } from "react";
+import Link from "next/link";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function Form() {
-  const router = useRouter()
+  const [error, setError] = useState("");
+
+  const router = useRouter();
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-    const response = await signIn('credentials', {
-      email: formData.get("email"),
-      password: formData.get("password"),
-      redirect: false
-    });
+    try {
+      const formData = new FormData(e.currentTarget);
+      const response = await signIn("credentials", {
+        email: formData.get("email"),
+        password: formData.get("password"),
+        redirect: false,
+      });
 
-    console.log({ response });
-    if (!response?.error) {
-      router.push('/')
-      router.refresh();
+      console.log({ response });
+      if (response?.error) {
+        setError("Invalid Credentials");
+        return;
+      } else {
+        router.push("/");
+        router.refresh();
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='flex flex-col gap-2 mx-auto max-w-md mt-10'
-    >
-      <input
-        className='border border-black text-black'
-        type='email'
-        name='email'
-      />
-      <input
-        className='border border-black text-black'
-        type='password'
-        name='password'
-      />
-      <button type='submit'>Login</button>
-    </form>
+    <div className='grid place-items-center h-screen'>
+      <div className='shadow-lg p-5 rounded-lg border-t-4'>
+        <h1 className='text-xl font-bold my-4'>Login</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+          <input
+            className='border border-black text-black'
+            placeholder='Email'
+            type='email'
+            name='email'
+          />
+          <input
+            className='border border-black text-black'
+            placeholder='Password'
+            type='password'
+            name='password'
+          />
+          <button
+            className='bg-green-500 text-white font-bold cursor-pointer px-6 py-2'
+            type='submit'
+          >
+            Login
+          </button>
+          {error && (
+            <div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2'>
+              {error}
+            </div>
+          )}
+
+          <Link className='text-sm mt-3 text-right' href={"/register"}>
+            Don't have an account? <span className='underline'>Register</span>
+          </Link>
+        </form>
+      </div>
+    </div>
   );
 }
--- app/register/form.tsx
+++ app/register/form.tsx
@@ -1,38 +1,69 @@
 "use client";
 
-import { FormEvent } from "react";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Form() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-    const response = await fetch("/api/auth/register", {
-      method: "POST",
-      body: JSON.stringify({
-        email: formData.get("email"),
-        password: formData.get("password"),
-      }),
-    });
-    console.log({ response });
+    try {
+      const formData = new FormData(e.currentTarget);
+      const response = await fetch("/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify({
+          email: formData.get("email"),
+          password: formData.get("password"),
+        }),
+      });
+      console.log({ response });
+    } catch (error) {
+      console.log("Error during registration", error);
+      setError("Error during registration");
+    }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='flex flex-col gap-2 mx-auto max-w-md mt-10'
-    >
-      <input
-        className='border border-black text-black'
-        type='email'
-        name='email'
-      />
-      <input
-        className='border border-black text-black'
-        type='password'
-        name='password'
-      />
-      <button type='submit'>Register</button>
-    </form>
+    <div className='grid place-items-center h-screen'>
+      <div className='shadow-lg p-5 rounded-lg border-t-4'>
+        <h1 className='text-xl font-bold my-4'>Register</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+          <input
+            className='border border-black text-black'
+            placeholder='Email'
+            type='email'
+            name='email'
+          />
+          <input
+            className='border border-black text-black'
+            placeholder='Password'
+            type='password'
+            name='password'
+          />
+          <button
+            className='bg-green-500 text-white w-fit text-sm cursor-pointer font-bold px-6 py-2'
+            type='submit'
+          >
+            Register
+          </button>
+
+          {error && (
+            <div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2'>
+              {error}
+            </div>
+          )}
+
+          <Link href='/login' className='text-sm mt-3 text-right'>
+            {" "}
+            Already have an account?
+            <span className='underline'>Login</span>
+          </Link>
+        </form>
+      </div>
+    </div>
   );
 }
