441d167f7c05cd4627ac44f3025d80600f46936a
JimmyMendy <jimmy.mendy@gmail.com>
2024-02-18 14:45:05 +0000

    final stage

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,14 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 import { sql } from "@vercel/postgres";
+import { z } from "zod";
 
 const handler = NextAuth({
   session: {
     strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
   },
   providers: [
     CredentialsProvider({
@@ -15,9 +19,19 @@ const handler = NextAuth({
       },
 
       async authorize(credentials, req) {
-        //
+        // const parsedCredential = z
+        //   .object({ email: z.string().email(), password: z.string().min(4) })
+        //   .safeParse(credentials);
+
+        // if (parsedCredential.success) {
+        //   const { email, password } = parsedCredential.data;
+
+        //   //
+
+        // }
+
         const response = await sql`
-        SELECT * FROM users WHERE email=${credentials?.email}`;
+          SELECT * FROM users WHERE email=${credentials?.email}`;
         const user = response.rows[0];
 
         const passwordCorrect = await compare(
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,12 @@
-import Form from "./form"
+import Form from "./form";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
-  return (
-    <Form/>
-  )
+export default async function LoginPage() {
+  const session = await getServerSession();
+
+  if (session) {
+    redirect("/");
+  }
+  return <Form />;
 }
